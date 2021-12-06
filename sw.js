6e9af27ad69e961d77f9566e5522713d692ae0a3
Soham1806 <deadly.soham@gmail.com>
2021-12-07 02:25:21 +0530

    made repository

--- sw.js
+++ sw.js
@@ -1,33 +1,103 @@
-// Service Worker
-const cacheName = "my-first-pwa";
-const filesToCache = [
-  "/",
-  "index.html",
-  "./js/index.js",
-  "./styles/styles.css"
-];
+// // Service Worker
+// const cacheName = "my-first-pwa";
+// const filesToCache = [
+//   "/",
+//   "index.html",
+//   "./index.js",
+//   "./sw.js",
+// ];
 
-self.addEventListener("install", e => {
-  console.log("[ServiceWorker**] Install");
+// self.addEventListener("install", e => {
+//   console.log("[ServiceWorker**] Install");
+//   e.waitUntil(
+//     caches.open(cacheName).then(cache => {
+//       console.log("[ServiceWorker**] Caching app shell");
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+// });
+
+
+
+// self.addEventListener("activate", event => {
+//    caches.keys().then(keyList => {
+//      return Promise.all(
+//        keyList.map(key => {
+//          if (key !== cacheName) {
+//            console.log("[ServiceWorker] - Removing old cache", key);
+//            return caches.delete(key);
+//          }
+//        })
+//      );
+//    });
+//  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*
+ *
+ *  Air Horner
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+const version = "0.6.18";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log("[ServiceWorker**] Caching app shell");
-      return cache.addAll(filesToCache);
+      return cache.addAll( [
+          "/",
+          "index.html",
+          "./index.js",
+          "./sw.js",
+        ])
+          .then(() => self.skipWaiting());
     })
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-
-self.addEventListener("activate", event => {
-   caches.keys().then(keyList => {
-     return Promise.all(
-       keyList.map(key => {
-         if (key !== cacheName) {
-           console.log("[ServiceWorker] - Removing old cache", key);
-           return caches.delete(key);
-         }
-       })
-     );
-   });
- });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
